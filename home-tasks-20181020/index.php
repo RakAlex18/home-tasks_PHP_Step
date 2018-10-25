594ed584d78fadb456acef4b27c492fa402430a4
RakAlex18 <novalex76@gmail.com>
2018-10-25 12:53:53 +0300

    ДЗ на проверку от 20 октября 2018

---
+++ home-tasks-20181020/index.php
@@ -0,0 +1,34 @@
+<?php
+date_default_timezone_set('Europe/Minsk');
+session_start();
+
+$_SESSION['text'] = "TEST";
+echo $_SESSION['text'] . "<br>";
+$_SESSION['test'] = "Hello!";
+?>
+<a href="hello.php">Перейти по ссылке</a><br>
+<?php
+if (!isset($_SESSION['counter'])) {
+    $_SESSION['counter'] = 0;
+    echo 'Вы еще не обновляли эту страницу ';
+} else {
+    $_SESSION['counter'] = $_SESSION['counter'] + 1;
+    echo 'Вы обновили эту страницу ' . $_SESSION['counter'] . ' раз!' . '<br>';
+}
+if (isset($_REQUEST['country'])) {
+    $_SESSION['country'] = $_REQUEST['country'];
+}
+//var_dump($_SESSION);
+?>
+
+<form action="" method="POST">
+    <input type="text" name="country" placeholder="Введите страну">
+    <input type="submit">
+</form>
+
+<?php
+if (!isset($_SESSION['user_inter'])) $_SESSION['user_inter'] = date('Y-m-d H:i:s');
+else $sec = time() - strtotime($_SESSION['user_inter']);
+echo 'Вы заходили на сайт ' . $sec . ' секунд назад';
+?>
+
---
+++ home-tasks-20181020/test.php
@@ -0,0 +1,5 @@
+<?php
+session_start();
+echo "Ваша страна ".$_SESSION['country'];
+?>
+
